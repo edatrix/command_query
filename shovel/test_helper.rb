5bcc1f678e406d436843875898770cdabd84bb75
Katrina Owen <katrina.owen@gmail.com>
2014-01-07 13:57:02 -0700

    Improve test helper

--- shovel/test_helper.rb
+++ shovel/test_helper.rb
@@ -9,11 +9,15 @@ class Minitest::Test
 end
 
 class FillInTheBlankTest < Minitest::Test
+  def __
+    "Fill in the blank."
+  end
+
   def equal_to_what(variable)
     begin
       raise
     rescue => e
-      failure = Exception.new("Make an assertion about `#{variable}`.")
+      failure = Exception.new("Replace the `equal_to_what` line with `assert_equal __, #{variable}`.")
       failure.set_backtrace e.backtrace[1..-1]
       raise failure
     end
