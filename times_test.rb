e00cdb1fd55acd563d60ad2b46335a9abd759a74
Katrina Owen <katrina.owen@gmail.com>
2014-01-09 11:06:13 -0700

    add rudimentary times test

---
+++ times_test.rb
@@ -0,0 +1,15 @@
+gem 'minitest', '~> 5.0'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class TimesTest < Minitest::Test
+
+  def test_do_it_again
+    happy_birthday = []
+    3.times do
+      happy_birthday << __
+    end
+    assert_equal ["spank 0", "spank 1", "spank 2"], happy_birthday
+  end
+
+end
