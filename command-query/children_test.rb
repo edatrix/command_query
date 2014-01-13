312bd17e2a0466cc1566a99335dff1ce7821c3df
Katrina Owen <katrina.owen@gmail.com>
2014-01-13 09:22:56 -0700

    Improve API on children test

--- command-query/children_test.rb
+++ command-query/children_test.rb
@@ -8,14 +8,14 @@ class ChildrenTest < Minitest::Test
   def test_nobody_is_the_eldest
     children = Children.new
 
-    assert_equal nil, children.eldest
+    assert_nil children.eldest
   end
 
   def test_the_eldest_of_one
     skip
     children = Children.new
     children << Child.new("Sarah", 5)
-    assert_equal "Sarah", children.eldest
+    assert_equal "Sarah", children.eldest.name
   end
 
   def test_the_eldest_of_several
@@ -24,6 +24,6 @@ class ChildrenTest < Minitest::Test
     children << Child.new("Robert", 2)
     children << Child.new("Fran", 8)
     children << Child.new("Hilbert", 4)
-    assert_equal "Fran", children.eldest
+    assert_equal "Fran", children.eldest.name
   end
 end
